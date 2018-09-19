ff5f342192d6d5779d2753d768d20db335e2ac59
Arnaud Lorenzi <alorenzi@ecocea.com>
2018-09-19 20:10:10 +0200

    init

---
+++ wad-test-bo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace wad_test_bo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
